7adbd1ffb54225cb4d89378b47b39908305395dc
Arnaud Thimel <a.thimel@gmail.com>
2011-01-31 13:39:43 +0000

    Create android archetype

---
+++ android/src/org/kimnono/Tarot.java
@@ -0,0 +1,15 @@
+package org.kimnono;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Tarot extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(org.kimnono.R.layout.main);
+    }
+}
