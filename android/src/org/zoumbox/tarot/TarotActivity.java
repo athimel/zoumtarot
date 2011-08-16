873509b36db1f2156bfddd43dd3b98991798f050
No author <no-author@xyz.com>
2011-08-16 23:46:42 +0000

    Use JSON to save/read data

--- android/src/org/zoumbox/tarot/TarotActivity.java
+++ android/src/org/zoumbox/tarot/TarotActivity.java
@@ -32,6 +32,7 @@ import com.google.common.base.Predicate;
 import com.google.common.collect.Collections2;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Sets;
+import com.google.gson.Gson;
 import com.thoughtworks.xstream.XStream;
 import com.thoughtworks.xstream.converters.basic.AbstractSingleValueConverter;
 import org.zoumbox.tarot.engine.Contract;
@@ -45,6 +46,7 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
@@ -59,40 +61,9 @@ public abstract class TarotActivity extends Activity {
 
     public static final String BOARDS_FILENAME_1_1 = "org.zoumbox.tarot-1.1-parties.xml";
     public static final String BOARD_FILENAME_1_2_PREFIX = "org.zoumbox.tarot-1.2-party-";
-    public static final String BOARD_FILENAME_1_2_SUFFIX = ".xml";
+    public static final String BOARD_FILENAME_1_2_SUFFIX = ".json";
     public static final String BOARD_FILENAME_1_2 = BOARD_FILENAME_1_2_PREFIX + "%d" + BOARD_FILENAME_1_2_SUFFIX;
     public static final Pattern BOARD_FILENAME_1_2_PATTERN = Pattern.compile(BOARD_FILENAME_1_2_PREFIX + "[0-9]*" + BOARD_FILENAME_1_2_SUFFIX);
-
-
-    public class SingleValueEnumConverter extends AbstractSingleValueConverter {
-        private final Class enumType;
-
-        public SingleValueEnumConverter(Class type) {
-            this.enumType = type;
-        }
-
-        public boolean canConvert(Class c) {
-            return c.equals(enumType);
-        }
-
-        public Object fromString(String value) {
-            return Enum.valueOf(enumType, value);
-        }
-    }
-
-    protected XStream getXStream() {
-        XStream result = new XStream();
-        result.alias("board", PlayerBoard.class);
-        result.alias("deal", Deal.class);
-        result.alias("contract", Contract.class);
-        result.alias("oudlers", Oudlers.class);
-        result.alias("handful", Handful.class);
-        result.registerConverter(new SingleValueEnumConverter(Contract.class));
-        result.registerConverter(new SingleValueEnumConverter(Oudlers.class));
-        result.registerConverter(new SingleValueEnumConverter(Handful.class));
-
-        return result;
-    }
 
     protected List<String> getSavedFileNames() {
         String[] files = fileList();
@@ -127,11 +98,9 @@ public abstract class TarotActivity extends Activity {
         List<PlayerBoard> result = Lists.newArrayList();
 
         if (!filesList.isEmpty()) {
-            XStream stream = getXStream();
-
             for (String fileName : filesList) {
                 try {
-                    PlayerBoard board = loadBoard(stream, fileName);
+                    PlayerBoard board = loadBoardFromFile(fileName);
                     result.add(board);
                 } catch (FileNotFoundException fnfe) {
                     Log.w(getClass().getSimpleName(), "Unable to load board: " + fileName, fnfe);
@@ -142,7 +111,7 @@ public abstract class TarotActivity extends Activity {
         return result;
     }
 
-    private PlayerBoard loadBoard(XStream stream, String fileName) throws FileNotFoundException {
+    private PlayerBoard loadBoardFromFile(String fileName) throws FileNotFoundException {
 
         Log.i(getClass().getSimpleName(), "Loading board from file: " + fileName);
 
@@ -150,7 +119,9 @@ public abstract class TarotActivity extends Activity {
         PlayerBoard result = null;
         try {
             is = openFileInput(fileName);
-            result = (PlayerBoard) stream.fromXML(is);
+            InputStreamReader reader = new InputStreamReader(is);
+            result = new Gson().fromJson(reader, PlayerBoard.class);
+            System.out.println(result);
         } finally {
             if (is != null) {
                 try {
@@ -164,16 +135,7 @@ public abstract class TarotActivity extends Activity {
         return result;
     }
 
-    protected void saveBoards(List<PlayerBoard> boards) throws IOException {
-
-        XStream stream = getXStream();
-
-        for (PlayerBoard board : boards) {
-            saveBoard(stream, board);
-        }
-    }
-
-    private void saveBoard(XStream stream, PlayerBoard board) throws IOException {
+    private void saveBoardToFile(PlayerBoard board) throws IOException {
 
         String fileName = String.format(BOARD_FILENAME_1_2, board.getCreationDate());
         Log.i(getClass().getSimpleName(), "Saving board to file: " + fileName);
@@ -181,8 +143,9 @@ public abstract class TarotActivity extends Activity {
         FileOutputStream os = null;
         try {
             os = openFileOutput(fileName, Context.MODE_WORLD_READABLE);
-            String xml = stream.toXML(board);
-            os.write(xml.getBytes());
+            String json = new Gson().toJson(board);
+            System.out.println(json);
+            os.write(json.getBytes());
         } finally {
             if (os != null) {
                 try {
@@ -197,9 +160,8 @@ public abstract class TarotActivity extends Activity {
 
     protected void saveBoard(PlayerBoard board) {
 
-        XStream stream = getXStream();
         try {
-            saveBoard(stream, board);
+            saveBoardToFile(board);
         } catch (IOException ioe) {
             Log.w(getClass().getSimpleName(), "Unable to save board", ioe);
         }
@@ -242,13 +204,11 @@ public abstract class TarotActivity extends Activity {
                 Set<String> filesSet = Sets.newHashSet(files);
                 if (filesSet.contains(BOARDS_FILENAME_1_1)) {
                     List<PlayerBoard11> playerBoard11s = loadBoards11(BOARDS_FILENAME_1_1);
-                    List<PlayerBoard> newList = Lists.newArrayList();
                     for (PlayerBoard11 board11 : playerBoard11s) {
                         Log.i(getClass().getSimpleName(), "Migrate board: " + board11.getScores());
                         PlayerBoard board = PlayerBoard.cloneIt(board11);
-                        newList.add(board);
+                        saveBoardToFile(board);
                     }
-                    saveBoards(newList);
                     deleteFile(BOARDS_FILENAME_1_1);
                 }
             }
@@ -256,6 +216,22 @@ public abstract class TarotActivity extends Activity {
             eee.printStackTrace();
         }
 
+    }
+
+    public class SingleValueEnumConverter extends AbstractSingleValueConverter {
+        private final Class enumType;
+
+        public SingleValueEnumConverter(Class type) {
+            this.enumType = type;
+        }
+
+        public boolean canConvert(Class c) {
+            return c.equals(enumType);
+        }
+
+        public Object fromString(String value) {
+            return Enum.valueOf(enumType, value);
+        }
     }
 
     protected XStream getXStream11() {
