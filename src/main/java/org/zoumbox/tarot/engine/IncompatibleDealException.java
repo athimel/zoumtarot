ae2f425a79a21ff54a72a87983e2557cfb1586f3
Arnaud Thimel <thimel@codelutin.com>
2016-10-22 23:54:37 +0200

    refs #1 Mise en place de Gradle

--- src/main/java/org/zoumbox/tarot/engine/IncompatibleDealException.java
+++ src/main/java/org/zoumbox/tarot/engine/IncompatibleDealException.java
@@ -7,23 +7,23 @@ public class IncompatibleDealException extends RuntimeException {
 
     private static final long serialVersionUID = 1554302050697890384L;
 
-    public IncompatibleDealException() {
-    }
+//    public IncompatibleDealException() {
+//    }
 
     public IncompatibleDealException(String s) {
         super(s);
     }
-
-    public IncompatibleDealException(String s, Throwable throwable) {
-        super(s, throwable);
-    }
-
-    public IncompatibleDealException(Throwable throwable) {
-        super(throwable);
-    }
-
-    public IncompatibleDealException(String s, Throwable throwable, boolean b, boolean b2) {
-        super(s, throwable, b, b2);
-    }
+//
+//    public IncompatibleDealException(String s, Throwable throwable) {
+//        super(s, throwable);
+//    }
+//
+//    public IncompatibleDealException(Throwable throwable) {
+//        super(throwable);
+//    }
+//
+//    public IncompatibleDealException(String s, Throwable throwable, boolean b, boolean b2) {
+//        super(s, throwable, b, b2);
+//    }
 
 }
--- src/main/java/org/zoumbox/tarot/engine/IncompleteDealException.java
+++ src/main/java/org/zoumbox/tarot/engine/IncompleteDealException.java
@@ -7,22 +7,22 @@ public class IncompleteDealException extends RuntimeException {
 
     private static final long serialVersionUID = -8476913472751032202L;
 
-    public IncompleteDealException() {
-    }
+//    public IncompleteDealException() {
+//    }
 
     public IncompleteDealException(String s) {
         super(s);
     }
 
-    public IncompleteDealException(String s, Throwable throwable) {
-        super(s, throwable);
-    }
-
-    public IncompleteDealException(Throwable throwable) {
-        super(throwable);
-    }
-
-    public IncompleteDealException(String s, Throwable throwable, boolean b, boolean b2) {
-        super(s, throwable, b, b2);
-    }
+//    public IncompleteDealException(String s, Throwable throwable) {
+//        super(s, throwable);
+//    }
+//
+//    public IncompleteDealException(Throwable throwable) {
+//        super(throwable);
+//    }
+//
+//    public IncompleteDealException(String s, Throwable throwable, boolean b, boolean b2) {
+//        super(s, throwable, b, b2);
+//    }
 }
